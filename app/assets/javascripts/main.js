d1260d5c7dff108526b8f19d4ad6ca421be17ea3
James Middlemiss <middlemissj.usa@gmail.com>
2015-08-17 11:24:39 -0400

    Functionality of click continue working. Straight from GreaterthanorEqualto

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  
+  var desiredtopmargin = $(window).height() * 0.08;
+
+  $("#content").css("padding-top", desiredtopmargin);
+  
+  readitall();
+  
+  opening();
+  
+  subjectline();
+  
+  pagetitle();
+  
+  $('body').css('display', 'block');
+  
+});
+
+function opening() {
+  $('a[data-opens]').click(function() {
+    
+    var openedby = $(this).attr('data-opens');
+    $('[data-openedby="' + openedby +'"]').removeClass('off').addClass('on').css('text-decoration','none');
+    
+    var closedby = $(this).attr('data-closes');
+    $('[data-closedby="' + closedby +'"]').css('display', 'none');
+    
+    $(this).addClass('clicked');
+    
+  });
+}
+
+function readitall() {
+  
+  $('a[data-finished]').click(function() {
+    setTimeout(function() {
+      $('head').append('<link rel="stylesheet" href="finished.css" type="text/css" />');
+    }, 5000);
+  });
+  
+}
+
+function subjectline() {
+
+  var emailsubjects = ["I wanted to tell you about a dream I had", "Flattery", "One day we will meet and then, then you will be sorry", "christ this website... something is wrong with you", "Have you ever thought about what happens after you die?", "This is something I have never told anyone, but I know you won’t judge me"];
+
+  var pickemailsubject = Math.floor(Math.random()*emailsubjects.length);
+
+  $('a[href="mailto:alantrotter@gmail.com"]').attr("href", "mailto:alantrotter@gmail.com?subject=" + emailsubjects[pickemailsubject]);
+
+}
+
+function pagetitle() {
+
+  var pagetitles = ["My Name Is James", "Howdy Y'all", "It's Snapchat, but for the written letter"]
+  
+  var pickpagetitle = Math.floor(Math.random()*pagetitles.length);
+  
+
+  $(document).attr('title', pagetitles[pickpagetitle]);
+  
+}
