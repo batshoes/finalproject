6ca5001ebcf477734888cbd732f7de7f39f0738f
James Middlemiss <middlemissj.usa@gmail.com>
2015-08-17 17:02:23 -0400

    Added Messages form saves, compiles message with email params.

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+class MessagesController < ApplicationController
+  def index
+    
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)    
+      if @message.valid?
+        @message.save
+        redirect_to root_path
+      else
+        flash[:notice] = "You wrong"
+      end
+  end
+
+  private 
+
+  def message_params
+    params.require(:message).permit(:to, :from, :title, :body, :sender_email)
+  end
+
+end
