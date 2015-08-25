02d0f36c28e90848e5a483aa7ebb97c6f629ac4e
James Middlemiss <middlemissj.usa@gmail.com>
2015-08-25 09:57:38 -0400

    added instructions to new page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,7 @@
 $(document).ready(function() {
   
-  var desiredtopmargin = $(window).height() * 0.08;
-
-  $("#content").css("padding-top", desiredtopmargin);
-  
-  readitall();
-  
   opening();
-  
-  // subjectline();
-  
+
   pagetitle();
   
   $('body').css('display', 'block');
@@ -30,25 +22,6 @@ function opening() {
   });
 }
 
-function readitall() {
-  
-  $('a[data-finished]').click(function() {
-    setTimeout(function() {
-      $('head').append('<link rel="stylesheet" href="finished.css" type="text/css" />');
-    }, 5000);
-  });
-  
-}
-
-// function subjectline() {
-
-//   var emailsubjects = ["I wanted to tell you about a dream I had", "Flattery", "One day we will meet and then, then you will be sorry", "christ this website... something is wrong with you", "Have you ever thought about what happens after you die?", "This is something I have never told anyone, but I know you won’t judge me"];
-
-//   var pickemailsubject = Math.floor(Math.random()*emailsubjects.length);
-
-//   $('a[href="mailto:j.o.middlemiss@gmail.com"]').attr("href", "mailto:j.o.middlemiss@gmail.com?subject=" + emailsubjects[pickemailsubject]);
-
-// }
 
 function pagetitle() {
 
@@ -64,23 +37,4 @@ function pagetitle() {
 $('#parsedOutput').on('click', function() {
   return $('span.hide').first().removeClass('hide');
 });
-
-$('#input').on('keyup', function() {
-  var i, j, n, numPipes, pipes, ref, val;
-  val = $(this).val();
-  $('#output').html(val);
-  pipes = val.match(/\|/g);
-  numPipes = (pipes != null ? pipes.length : void 0) || 0;
-  $("#pipeCount").text(numPipes);
-  if (!((pipes != null) && numPipes % 2 === 0)) {
-    return;
-  }
-  n = (numPipes / 2) - 1;
-  for (i = j = 0, ref = n; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-    val = val.replace(/\|/, "<span class='hide'>");
-  }
-  val = val.replace(/\|/g, "</span>");
-  return $('#parsedOutput').html(val);
-});
-
 
--- lib/mail_sender.rb
+++ lib/mail_sender.rb
@@ -10,15 +10,15 @@ class MailSender
     @your_name = params['from']
     @access_token = params['access_token']
 
-
-    @confirmation = "<p>Hi!, #{@name} You've gotten a Fleet from #{@your_name}.</p>"
+    
+    @confirmation = "<p>Hi!, #{@name} You've gotten a PenMail from #{@your_name}.</p>"
     @confirmation = "#{@confirmation}" + "Click here! http://localhost:3000/messages/#{@access_token}"
     
     mandrill = Mandrill::API.new ENV['MANDRILL_APIKEY']
      message = {to: [{"type"  =>"to",
                       "email" =>"#{@email_to}",
                       "name"  =>"#{@name}" }],
-                subject: "You've Got a Fleet",
+                subject: "You've Got a PenMail",
                 from_email: "#{@email_from}",
                 html: "#{@confirmation} " 
                }
